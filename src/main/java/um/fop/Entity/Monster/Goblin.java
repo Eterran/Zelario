4be6ce5f4d4257858661839ed9105fd731d11449
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-11-26 01:20:36 +0800

    Update Goblin.java

--- src/main/java/um/fop/Entity/Monster/Goblin.java
+++ src/main/java/um/fop/Entity/Monster/Goblin.java
@@ -1,5 +1,13 @@
 package Entity.Monster;
 
 public class Goblin extends Monster{
-    
+    public Goblin(int initialHP, int initialMP, int initialPhysicalAttack,
+                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense) {
+       int HP = 40;
+       int MP = 0;
+       int physicalAttack = 10;
+       int magicalAttack = 0;
+       int physicalDefense = 5;
+       int magicalDefense = 2;
+    }
 }
