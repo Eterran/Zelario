a45af3f31727275e3f4209f25eb598990bd94861
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-11-26 01:30:28 +0800

    Update Entity.java

--- src/main/java/um/fop/Entity/Entity.java
+++ src/main/java/um/fop/Entity/Entity.java
@@ -2,5 +2,18 @@ package Entity;
 
 
 public class Entity {
-    
+     public Entity(int HP, int MP, int physicalAttack,
+                 int magicalAttack, int physicalDefense, int magicalDefense,
+                 int skill1, int skill2, int CDSkillOne, int CDSkillTwo) {
+        this.HP = HP;
+        this.MP = MP;
+        this.physicalAttack = physicalAttack;
+        this.magicalAttack = magicalAttack;
+        this.physicalDefense = physicalDefense;
+        this.magicalDefense = magicalDefense;
+        this.skill1 = skill1;
+        this.skill2 = skill2;
+        this.CDSkillOne = CDSkillOne;
+        this.CDSkillTwo = CDSkillTwo;
+    }
 }
