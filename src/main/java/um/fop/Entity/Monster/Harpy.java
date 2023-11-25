52473968a743a281ca7fc61bc3ec3729f1aaac4a
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-11-26 01:27:16 +0800

    Update Harpy.java

--- src/main/java/um/fop/Entity/Monster/Harpy.java
+++ src/main/java/um/fop/Entity/Monster/Harpy.java
@@ -1,5 +1,17 @@
 package Entity.Monster;
 
 public class Harpy extends Monster {
-    
+     public Harpy(int initialHP, int initialMP, int initialPhysicalAttack,
+                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense, int skill1, int skill2, int CDSkillOne, int CDSkillTwo) {
+       int HP = 60;
+       int MP = 20;
+       int physicalAttack = 14;
+       int magicalAttack = 10;
+       int physicalDefense = 8;
+       int magicalDefense = 8;
+       int skill1 = 100;
+       int skill2 = 150;
+       int CDSKillOne = 4;
+       int CDSkillTwo = 6;
+    }
 }
