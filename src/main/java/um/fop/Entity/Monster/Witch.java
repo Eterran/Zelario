660bffb44e75eed8d03f32e6f07dd7ec4212e371
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-11-29 00:19:19 +0800

    Update Witch.java

--- src/main/java/um/fop/Entity/Monster/Witch.java
+++ src/main/java/um/fop/Entity/Monster/Witch.java
@@ -1,5 +1,15 @@
 package Entity.Monster;
 
 public class Witch extends Monster{
-    
+    public Entity(int initialHP, int initialMP, int initialPhysicalAttack,
+                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense) {
+       int HP = 40;
+       int MP = 0;
+       int physicalAttack = 10;
+       int magicalAttack = 0;
+       int physicalDefense = 5;
+       int magicalDefense = 2;
+        int skillOne = 100
+        int 
+    }
 }
