f946eb0717b2ec9fa3e9f789ea240b0a64a73fca
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-11-29 02:29:43 +0800

    Update Warrior.java

--- src/main/java/um/fop/Entity/Player/Warrior.java
+++ src/main/java/um/fop/Entity/Player/Warrior.java
@@ -1,5 +1,21 @@
 package Entity.Player;
 
 public class Warrior extends Archetypes{
-    
+    public Entity(String name, int initialHP, int initialMP, int initialPhysicalAttack,
+                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense, int skill1, int skill2, int CDSkillOne, int CDSkillTwo) {
+       String name = "Warrior";
+       int HP = 250;
+       int MP = 75;
+       int physicalAttack = 60;
+       int magicalAttack = 40;
+       int physicalDefense = 50;
+       int magicalDefense = 40;
+       int skill1 = 50; //Buff Attack
+       int skill2 = 150; //Furious strike
+       int skill3 = 0; //Shield wall (reducing damage taken)
+       int CDSKillOne = 3;
+       int CDSkillTwo = 4;
+       int CDSkillThree = 6;
+         
+    }
 }
