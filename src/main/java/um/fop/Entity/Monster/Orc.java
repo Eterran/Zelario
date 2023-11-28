84d9e281765f77843751ad7e40c4bc20998ec8f3
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-11-29 00:17:22 +0800

    Update Orc.java

--- src/main/java/um/fop/Entity/Monster/Orc.java
+++ src/main/java/um/fop/Entity/Monster/Orc.java
@@ -1,5 +1,13 @@
 package Entity.Monster;
 
 public class Orc extends Monster{
-    
+    public Entity(int initialHP, int initialMP, int initialPhysicalAttack,
+                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense) {
+       int HP = 70;
+       int MP = 0;
+       int physicalAttack = 15;
+       int magicalAttack = 0;
+       int physicalDefense = 9;
+       int magicalDefense = 4;
+    }
 }
