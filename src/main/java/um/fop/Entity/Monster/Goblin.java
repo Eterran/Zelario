0d9f561de2315054681eb5301ae12862ad8555a9
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-11-30 04:02:19 +0800

    Update Goblin.java

--- src/main/java/um/fop/Entity/Monster/Goblin.java
+++ src/main/java/um/fop/Entity/Monster/Goblin.java
@@ -1,14 +1,31 @@
-package Entity.Monster;
+package Monster;
 
-public class Goblin extends Monster{
-    public Entity(String name, int initialHP, int initialMP, int initialPhysicalAttack,
-                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense) {
-       String name = "Goblim";
-       int HP = 40;
-       int MP = 0;
-       int physicalAttack = 10;
-       int magicalAttack = 0;
-       int physicalDefense = 5;
-       int magicalDefense = 2;
+public class Goblin {
+    private String monsterName;
+    private int HP;
+    private int MP;
+    private int physicalAttack;
+    private int magicalAttack;
+    private int physicalDefence;
+    private int magicalDefence;
+
+    // Constructor
+    public Goblin(String name, int initialHP, int initialMP, int initialPhysicalAttack,
+                  int initialMagicalAttack, int initialPhysicalDefence, int initialMagicalDefence) {
+        monsterName = name;
+        HP = initialHP;
+        MP = initialMP;
+        physicalAttack = initialPhysicalAttack;
+        magicalAttack = initialMagicalAttack;
+        physicalDefence = initialPhysicalDefence;
+        magicalDefence = initialMagicalDefence;
+    }
+    
+    public void damagedealt(){
+       HP = (int)(physicalAttack * (1.0 - physicalDefence / 100.0)); //enemy HP
+    }
+      
+       public void damagetaken(){
+       HP = (int)(physicalAttack * (1.0 - physicalDefence / 100.0)); //enemy physical attack
     }
 }
