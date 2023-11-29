5e0d9783acd5abd838c5e612d03f105985fe0538
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-11-30 04:02:36 +0800

    Update Entity.java

--- src/main/java/um/fop/Entity/Entity.java
+++ src/main/java/um/fop/Entity/Entity.java
@@ -1,22 +1,90 @@
-package Entity;
-
+package com.mycompany.textadventure2;
 
 public class Entity {
-     public Entity(String name, int HP, int MP, int physicalAttack,
-                 int magicalAttack, int physicalDefense, int magicalDefense,
-                 int skill1, int skill2, int CDSkillOne, int CDSkillTwo) {
+    // Instance variables
+    private String name;
+    private int HP;
+    private int MP;
+    private int physicalAttack;
+    private int magicalAttack;
+    private int physicalDefence;
+    private int magicalDefence;
+    private int skill1;
+    private int skill2;
+    private int skill3;
+    private int CDSkillOne;
+    private int CDSkillTwo;
+    private int CDSkillThree;
+
+    
+    public Entity(String name, int HP, int MP, int physicalAttack,
+                  int magicalAttack, int physicalDefence, int magicalDefence,
+                  int skill1, int skill2, int skill3, int CDSkillOne, int CDSkillTwo, int CDSkillThree) {
         this.name = name;
         this.HP = HP;
         this.MP = MP;
         this.physicalAttack = physicalAttack;
         this.magicalAttack = magicalAttack;
-        this.physicalDefense = physicalDefense;
-        this.magicalDefense = magicalDefense;
+        this.physicalDefence = physicalDefence;
+        this.magicalDefence = magicalDefence;
         this.skill1 = skill1;
         this.skill2 = skill2;
-        this.skill3 = skill3;   
+        this.skill3 = skill3;
         this.CDSkillOne = CDSkillOne;
         this.CDSkillTwo = CDSkillTwo;
         this.CDSkillThree = CDSkillThree;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public int getMP() {
+        return MP;
+    }
+
+   public int getphysicalAttack() {
+        return physicalAttack;
+    }
+   
+   public int getmagicalAttack() {
+        return magicalAttack;
+    }
+   
+   public int getphysicalDefence() {
+        return physicalDefence;
+    }
+   
+   public int getmagicalDefence() {
+        return magicalDefence;
+    }
+   
+   public int getskil1() {
+        return skill1;
+    }
+   
+   public int getskil2() {
+        return skill2;
+    }
+   
+   public int getskil3() {
+        return skill3;
+    }
+   
+   public int CDSkillOne() {
+        return CDSkillOne;
+    }
+   
+   public int CDSkillTwo() {
+        return CDSkillTwo;
+    }
+   
+   public int CDSkillThree() {
+        return CDSkillThree;
     }
 }
