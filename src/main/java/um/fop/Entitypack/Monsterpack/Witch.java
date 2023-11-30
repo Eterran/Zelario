86fd75912e48d2b0b851d2b332772da1914cfcd9
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-12-01 00:49:43 +0800

    Update Witch.java

--- src/main/java/um/fop/Entitypack/Monsterpack/Witch.java
+++ src/main/java/um/fop/Entitypack/Monsterpack/Witch.java
@@ -1,17 +1,17 @@
 package Entitypack.Monsterpack;
 
 public class Witch extends Monster{
-    public Entity(String name, int initialHP, int initialMP, int initialPhysicalAttack,
-                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense) {
-        String name = "Witch";
-       int HP = 40;
-       int MP = 0;
-       int physicalAttack = 10;
-       int magicalAttack = 0;
-       int physicalDefense = 5;
-       int magicalDefense = 2;
-       int skill1 = 100;
-       int CDSKillOne = 4;
+    name = "Witch";
+        HP = 40;
+        MP = 0;
+        physicalAttack = 10;
+        magicalAttack = 0;
+        physicalDefense = 5;
+        magicalDefense = 2;
+        skillOne = 100; //Fireball
+        CDSKillOne = 4;
        
+    public void useSkillone(){
+       HP = (int)(skillOne * (1.0 - magicalDefence / 100.0)); //enemy HP
     }
 }
