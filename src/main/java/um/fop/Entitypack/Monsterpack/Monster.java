24ae6b134bca19a445858cfdddaf851458ef0985
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-12-01 01:37:50 +0800

    Update Monster.java

--- src/main/java/um/fop/Entitypack/Monsterpack/Monster.java
+++ src/main/java/um/fop/Entitypack/Monsterpack/Monster.java
@@ -1,9 +1,5 @@
 package Entitypack.Monsterpack;
 import Entitypack.Entity;
 public class Monster extends Entity{
-    Goblin goblinObj = new Goblin("Goblin", 40, 0, 10, 0, 5, 2);
-        Harpy harpyObj = new Harpy("Harpy", 60, 20, 14, 10, 8, 8, 100, 150, 4, 6);
-        Witch witchObj = new Witch("Witch", 40, 0, 10, 0, 5, 2, 100, 4);
-        Orc orcObj = new Orc("Orc", 70, 0, 15, 0, 9, 4);
-        Skeleton skeletonObj = new Skeleton("Skeleton", 60, 0, 12, 0, 7, 3);
+   
 }
