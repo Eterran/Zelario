30e4b940c90124ec09f58d2e261e6ae8314bd83f
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-12-01 00:26:27 +0800

    Update Skeleton.java

--- src/main/java/um/fop/Entitypack/Monsterpack/Skeleton.java
+++ src/main/java/um/fop/Entitypack/Monsterpack/Skeleton.java
@@ -1,14 +1,13 @@
 package Entitypack.Monsterpack;
 
 public class Skeleton extends Monster{
-    public Entity(String name, int initialHP, int initialMP, int initialPhysicalAttack,
-                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense) {
-        String name = "Skeleton";
-       int HP = 60;
-       int MP = 0;
-       int physicalAttack = 12;
-       int magicalAttack = 0;
-       int physicalDefense = 7;
-       int magicalDefense = 3;
+    public Skeleton {
+        Monstername = "Skeleton";
+        HP = 60;
+        MP = 0;
+        physicalAttack = 12;
+        magicalAttack = 0;
+        physicalDefense = 7;
+        magicalDefense = 3;
     }
 }
