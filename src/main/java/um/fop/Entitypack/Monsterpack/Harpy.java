f58fd661ca9de2c2fc6c347e0cf5c6716e24e811
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-12-01 00:46:27 +0800

    Update Harpy.java

--- src/main/java/um/fop/Entitypack/Monsterpack/Harpy.java
+++ src/main/java/um/fop/Entitypack/Monsterpack/Harpy.java
@@ -1,18 +1,23 @@
 package Entitypack.Monsterpack;
 
 public class Harpy extends Monster {
-     public Entity(String name, int initialHP, int initialMP, int initialPhysicalAttack,
-                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense, int skill1, int skill2, int CDSkillOne, int CDSkillTwo) {
-       String name = "Harpy";
-       int HP = 60;
-       int MP = 20;
-       int physicalAttack = 14;
-       int magicalAttack = 10;
-       int physicalDefense = 8;
-       int magicalDefense = 8;
-       int skill1 = 100;
-       int skill2 = 150;
-       int CDSKillOne = 4;
-       int CDSkillTwo = 6;
+     name = "Harpy";
+       HP = 60;
+       MP = 20;
+       physicalAttack = 14;
+       magicalAttack = 10;
+       physicalDefense = 8;
+       magicalDefense = 8;
+       skillOne = 150; //Talon strike
+       skillTwo = 150; //Wind Gust
+       CDSkillOne = 4;
+       CDSkillTwo = 6;
+       
+       public void useSkillOne() {
+        HP = (int)(skillOne * (1.0 - physicalDefence / 100.0)); //player HP
+    }
+    
+    public void useSkilltwo(){
+       HP = (int)(skillOne * (1.0 - magicalDefence / 100.0)); //player HP, and add weakend to player
     }
 }
