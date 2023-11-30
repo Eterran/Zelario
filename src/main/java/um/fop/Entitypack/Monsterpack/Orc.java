332b8f6a82416d853caa8ef3690b69a13c8df9d4
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-12-01 00:47:24 +0800

    Update Orc.java

--- src/main/java/um/fop/Entitypack/Monsterpack/Orc.java
+++ src/main/java/um/fop/Entitypack/Monsterpack/Orc.java
@@ -1,14 +1,12 @@
 package Entitypack.Monsterpack;
 
 public class Orc extends Monster{
-    public Entity(String name, int initialHP, int initialMP, int initialPhysicalAttack,
-                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense) {
-       String name = "Orc";
-       int HP = 70;
-       int MP = 0;
-       int physicalAttack = 15;
-       int magicalAttack = 0;
-       int physicalDefense = 9;
-       int magicalDefense = 4;
-    }
+    name = "Orc";
+        HP = 70;
+        MP = 0;
+        physicalAttack = 15;
+        magicalAttack = 0;
+        physicalDefense = 9;
+        magicalDefense = 4;
+
 }
