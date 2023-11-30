45d84f337a89e219b8a67c8f690415d5de2fc444
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-12-01 01:30:46 +0800

    Update Warrior.java

--- src/main/java/um/fop/Entitypack/Playerpack/Warrior.java
+++ src/main/java/um/fop/Entitypack/Playerpack/Warrior.java
@@ -1,21 +1,34 @@
 package Entitypack.Playerpack;
 
 public class Warrior extends Archetypes{
-    public Warrior(String name, int initialHP, int initialMP, int initialPhysicalAttack,
-                  int initialMagicalAttack, int initialPhysicalDefense, int initialMagicalDefense, int skill1, int skill2, int skill3, int CDSkill1, int CDSkill2, int CDSkill3) {
-       String name = "Warrior";
-       int HP = 250;
-       int MP = 75;
-       int physicalAttack = 60;
-       int magicalAttack = 40;
-       int physicalDefense = 50;
-       int magicalDefense = 40;
-       int skill1 = 50; //Buff Attack
-       int skill2 = 150; //Furious strike
-       int skill3 = 0; //Shield wall (reducing damage taken)
-       int CDSKillOne = 3;
-       int CDSkillTwo = 4;
-       int CDSkillThree = 6;
+    name = "Warrior";
+       HP = 250;
+       MP = 75;
+       physicalAttack = 60;
+       magicalAttack = 40;
+       physicalDefense = 50;
+       magicalDefense = 40;
+       skillOne = 50; //"Justice"
+       skillTwo = 200; //Furious strike
+       skillThree = 50; //Shield wall (reducing damage taken)
+       CDSKillOne = 2;
+       CDSkillTwo = 4;
+       CDSkillThree = 20;
          
+     public void useSkillOne() {
+        
+        physicalAttack =(getPhysicalAttack() + getSkill1());
+    }
+
+    public void useSkillTwo() {
+       
+         damageDealt = (int) (getSkill2() * (1.0 - getPhysicalDefence() / 100.0));
+        
+    }
+
+    public void useSkillThree() {
+        
+         damageTaken = (int)(damageDealt() * getSkill3()/100);
+        
     }
 }
