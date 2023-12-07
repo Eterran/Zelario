790fa904559adb31b2774325b45e1d2833d688b1
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2023-12-07 11:10:04 +0800

    Update Witch.java

--- src/main/java/um/fop/Entitypack/Monsterpack/Witch.java
+++ src/main/java/um/fop/Entitypack/Monsterpack/Witch.java
@@ -7,14 +7,14 @@ public class Witch extends Monster{
         MP = 0;
         physicalAttack = 10;
         magicalAttack = 0;
-        physicalDefense = 5;
-        magicalDefense = 2;
+        physicalDefence = 5;
+        magicalDefence = 2;
         skillOne = 100; //Fireball
         CDSKillOne = 4;
        
-   public void useSkillOne() {
+   public void useSkillOne(Entity target) {
       
-        int damageDealt = (int) (getSkill1() * (1.0 - getMagicalDefence() / 100.0));
+        int damageDealt = (int) (this.getSkill1() * (1.0 - target.getMagicalDefence() / 100.0));
       
     }
 }
