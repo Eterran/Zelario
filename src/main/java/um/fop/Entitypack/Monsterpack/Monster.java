dfe8fb6dd5803c8693cc360b582772da07397163
Jack <yeawjiaming04@gmail.com>
2023-12-25 20:07:05 +0800

    changes

--- src/main/java/um/fop/Entitypack/Monsterpack/Monster.java
+++ src/main/java/um/fop/Entitypack/Monsterpack/Monster.java
@@ -2,4 +2,9 @@ package Entitypack.Monsterpack;
 import Entitypack.Entity;
 public class Monster extends Entity{
    
+    public int normalAttack(Entity target) {//physical normal attack for monster
+        int dmg = (int) (this.getPhysicalAttack() * (1.0 - target.getPhysicalDefence() / 100.0)); 
+        target.damageTaken(dmg);
+        return dmg;
+    }
 }
