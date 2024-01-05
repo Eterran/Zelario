36f05d9f0bdb729af25812d384cb3e0e5f828c2f
Yeawjiaming <147384564+Yeawjiaming@users.noreply.github.com>
2024-01-06 06:07:10 +0800

    Update Archer.java

--- src/main/java/um/fop/Entitypack/Playerpack/Archer.java
+++ src/main/java/um/fop/Entitypack/Playerpack/Archer.java
@@ -27,32 +27,32 @@ public class Archer extends Player {
                     this.magicalAttack = Integer.parseInt(stats[4]);
                     this.physicalDefence = Integer.parseInt(stats[5]);
                     this.magicalDefence = Integer.parseInt(stats[6]);
+                    this.skillOneName = stats[7];
+                    this.skill1Description = stats[8];
+                    this.skillTwoName = stats[9];
+                    this.skill2Description = stats[10];
+                    this.skillThreeName = stats[11];
+                    this.skill3Description = stats[12];
+                    this.skill1 = Integer.parseInt(stats[13]); // "Buff attack for one turn" LV5 unlock
+                    this.skill2 = Integer.parseInt(stats[14]); // Rapid arrow LV10 Unlock
+                    this.skill3 = Integer.parseInt(stats[15]); // Trick shot LV30 Unlock
+                    this.skill1Mp = Integer.parseInt(stats[16]); 
+                    this.skill2Mp = Integer.parseInt(stats[17]); 
+                    this.skill3Mp = Integer.parseInt(stats[18]); 
+                    this.MaxCDSkill1 = Integer.parseInt(stats[19]);
+                    this.MaxCDSkill2 = Integer.parseInt(stats[20]);
+                    this.MaxCDSkill3 = Integer.parseInt(stats[21]);
+                    this.CDSkill1 = Integer.parseInt(stats[19]);
+                    this.CDSkill2 = Integer.parseInt(stats[20]);
+                    this.CDSkill3 = Integer.parseInt(stats[21]);
+          
                }
 
 
           }catch(FileNotFoundException e){
                System.out.println("File was not found");
           }
-          
-        
-          this.skillOneName = "Precise\t";
-          this.skill1Description = "Buff physical attack for 2 rounds.";
-          this.skillTwoName = "Rapid shot\t";
-          this.skill2Description = "Fires a flurry of arrows at the target, dealing moderate damage.";
-          this.skillThreeName = "Trick Shot\t";
-          this.skill3Description = "Executes a precise shot that can pierce the enemies, causing heavy damage to the enemy.";
-          this.skill1 = 50; // "Buff attack for one turn" LV5 unlock
-          this.skill2 = 100; // Rapid arrow LV10 Unlock
-          this.skill3 = 300; // Trick shot LV30 Unlock
-          this.skill1Mp = 30; 
-          this.skill2Mp = 20; 
-          this.skill3Mp = 60; 
-          this.MaxCDSkill1 = 4;
-          this.MaxCDSkill2 = 2;
-          this.MaxCDSkill3 = 6;
-          this.CDSkill1 = 4;
-          this.CDSkill2 = 2;
-          this.CDSkill3 = 6;
+              
      }
 
      public int useSkill1(Entity target) {
