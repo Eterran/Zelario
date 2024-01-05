b2f749bb1b30fe9042f9dbdd6c85eb0a4ca37039
Eterran <146816991+Eterran@users.noreply.github.com>
2024-01-05 22:56:55 +0800

    Added intro story

--- src/main/java/um/fop/Start.java
+++ src/main/java/um/fop/Start.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
+
 import java.awt.event.*;
 
 import Entitypack.*;
@@ -21,7 +24,7 @@ public class Start {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(815, 810);
         frame.setLocationRelativeTo(null);
-        //frame.setResizable(false);
+        // frame.setResizable(false);
 
         // Set the layout of the JFrame's content pane
         frame.getContentPane().setLayout(new BorderLayout());
@@ -72,20 +75,19 @@ public class Start {
             }
         });
 
-
         userInputField.setBackground(Color.BLACK);
-        userInputField.setBorder(null); 
-        userInputField.setFont(new Font("Monospaced", Font.PLAIN, 14)); 
+        userInputField.setBorder(null);
+        userInputField.setFont(new Font("Monospaced", Font.PLAIN, 14));
 
         JScrollPane scrollPane = new JScrollPane(textPane);
         scrollPane.setBorder(null);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
         scrollPane.setBorder(null);
-        
+
         // Adding components to the frame
         frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-        
+
         // Redirecting System.out to the text area
         PrintStream printStream = new PrintStream(new CustomOutputStream(textPane));
         System.setOut(printStream);
@@ -106,135 +108,137 @@ public class Start {
         // String username = "null", pw = "null";
         // Connection conn = null;
         // try {
-        //     conn = SQL.getConnection();
+        // conn = SQL.getConnection();
         // } catch (Exception e) {
-        //     System.out.println("Unable to establish connection with the server.");
-        //     return;
+        // System.out.println("Unable to establish connection with the server.");
+        // return;
         // }
         // try {
-        //     LoginMenu.startingScreen();
+        // LoginMenu.startingScreen();
         // } catch (IOException e) {
-        //     System.out.println("File not found. (Starting Screen)");
+        // System.out.println("File not found. (Starting Screen)");
         // }
 
         // String input = "";
-		// try {
-		// 	do {
-		// 		input = consoleToGUI.getNextInput();
-		// 		if (!input.matches("^[1-2]$")) {
-		// 		}
-		// 	} while (!input.matches("^[1-2]$"));
+        // try {
+        // do {
+        // input = consoleToGUI.getNextInput();
+        // if (!input.matches("^[1-2]$")) {
+        // }
+        // } while (!input.matches("^[1-2]$"));
         // } catch (InterruptedException e) {
-            
+
         // }
 
         // switch (input){
-        //     case "1":
-        //         while(true){
-        //             try {
-        //                 System.out.println("Register");
-        //                     System.out.print("+");
+        // case "1":
+        // while(true){
+        // try {
+        // System.out.println("Register");
+        // System.out.print("+");
 
-        //                     int width = 86;
-        //                     for (int a=0; a<width;a++)
-        //                         System.out.print("-");
-        //                     System.out.println("+");
-        //                 System.out.println("Enter your username: ");
-        //                 try {
-        //                     username = consoleToGUI.getNextInput();
-        //                 } catch (Exception e) {
-        //                     e.printStackTrace();
-        //                 }
-        //                 System.out.println("Enter your password: ");
-        //                 try {
-        //                     pw = consoleToGUI.getNextInput();
-        //                 } catch (Exception e) {
-        //                     e.printStackTrace();
-        //                 }
-        //                 if (SQL.usernameExists(conn, username)) {
-        //                     System.out.println("Username already exists. Please choose a different username.");
-        //                     continue;
-        //                 }
-        //                 SQL.registerUser(conn, username, pw);
-        //                 break;
-        //             } catch (SQLException e) {
-        //                 System.out.println("Error while registering user or connecting to the database.");
-        //             }
-        //         }
-        //         break;
-        //     case "2":
-        //     while (true){
-        //         System.out.println("Login");
-        //         System.out.println("Enter your username: ");
-        //         try {
-        //             username = consoleToGUI.getNextInput();
-        //         } catch (Exception e) {
-        //             e.printStackTrace();
-        //         }
-        //         System.out.println("Enter your password: ");
-        //         try {
-        //             pw = consoleToGUI.getNextInput();
-        //         } catch (Exception e) {
-        //             e.printStackTrace();
-        //         }
-        //             System.out.print("+");
+        // int width = 86;
+        // for (int a=0; a<width;a++)
+        // System.out.print("-");
+        // System.out.println("+");
+        // System.out.println("Enter your username: ");
+        // try {
+        // username = consoleToGUI.getNextInput();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // System.out.println("Enter your password: ");
+        // try {
+        // pw = consoleToGUI.getNextInput();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // if (SQL.usernameExists(conn, username)) {
+        // System.out.println("Username already exists. Please choose a different
+        // username.");
+        // continue;
+        // }
+        // SQL.registerUser(conn, username, pw);
+        // break;
+        // } catch (SQLException e) {
+        // System.out.println("Error while registering user or connecting to the
+        // database.");
+        // }
+        // }
+        // break;
+        // case "2":
+        // while (true){
+        // System.out.println("Login");
+        // System.out.println("Enter your username: ");
+        // try {
+        // username = consoleToGUI.getNextInput();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // System.out.println("Enter your password: ");
+        // try {
+        // pw = consoleToGUI.getNextInput();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // System.out.print("+");
 
-        //             int width = 86;
-        //             for (int a=0; a<width;a++)
-        //                 System.out.print("-");
-        //             System.out.println("+");
-        //         try {
-        //             if (!(SQL.usernameExists(conn, username))) {
-        //                 System.out.println("Username does not exists, please try again.");
-        //             }
-        //         } catch (SQLException e) {
-        //             System.out.println("Unable to establish connection with the server.");
-        //             return;
-        //         }
-        //         String existingPassword;
-        //         try {
-        //             existingPassword = SQL.getPassword(conn, username);
-        //         } catch (Exception e) {
-        //             System.out.println("Unable to establish connection with the server.");
-        //             return;
-        //         }
-        //         while(true){
-        //             if (existingPassword == null) {
-        //                 System.out.println("Username does not exist.");
-        //                 return;
-        //             } else if (!existingPassword.equals(pw)) {
-        //                     System.out.println("Incorrect password.");
-        //                     System.out.println("Enter your password: ");
-        //                     try {
-        //                         pw = consoleToGUI.getNextInput();
-        //                     } catch (Exception e) {
-        //                         e.printStackTrace();
-        //                     }
-        //             } else {
-        //                 break;
-        //             }
-        //         }
-        //         break;
-        //     }
+        // int width = 86;
+        // for (int a=0; a<width;a++)
+        // System.out.print("-");
+        // System.out.println("+");
+        // try {
+        // if (!(SQL.usernameExists(conn, username))) {
+        // System.out.println("Username does not exists, please try again.");
+        // }
+        // } catch (SQLException e) {
+        // System.out.println("Unable to establish connection with the server.");
+        // return;
+        // }
+        // String existingPassword;
+        // try {
+        // existingPassword = SQL.getPassword(conn, username);
+        // } catch (Exception e) {
+        // System.out.println("Unable to establish connection with the server.");
+        // return;
+        // }
+        // while(true){
+        // if (existingPassword == null) {
+        // System.out.println("Username does not exist.");
+        // return;
+        // } else if (!existingPassword.equals(pw)) {
+        // System.out.println("Incorrect password.");
+        // System.out.println("Enter your password: ");
+        // try {
+        // pw = consoleToGUI.getNextInput();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // } else {
+        // break;
+        // }
+        // }
+        // break;
+        // }
         // }
         // try {
-        //     String saveFilePath = SQL.getSaveFile(conn, username);
-        //     File saveFile = new File(saveFilePath);
-        //     if (saveFile.exists()) {
-        //         try(BufferedReader r = new BufferedReader(new FileReader(saveFile))){
-        //             String line;
-        //             while((line = r.readLine()) != null){
-        //                 System.out.println(line);
-        //             }
-        //         } catch(IOException e){
-        //             System.out.println("The file does not exists. (Somehow)");
-        //         }
-        //     }
+        // String saveFilePath = SQL.getSaveFile(conn, username);
+        // File saveFile = new File(saveFilePath);
+        // if (saveFile.exists()) {
+        // try(BufferedReader r = new BufferedReader(new FileReader(saveFile))){
+        // String line;
+        // while((line = r.readLine()) != null){
+        // System.out.println(line);
+        // }
+        // } catch(IOException e){
+        // System.out.println("The file does not exists. (Somehow)");
+        // }
+        // }
         // } catch (SQLException e) {
-        //     System.out.println("Unable to establish connection with the server.");
+        // System.out.println("Unable to establish connection with the server.");
         // }
 
-        while(PickCharacter.heroChoice == -1){
+        while (PickCharacter.heroChoice == -1) {
             try {
                 PickCharacter.pickCharacterMenu(consoleToGUI, userInputField, textPane);
             } catch (FileNotFoundException e) {
@@ -242,21 +246,51 @@ public class Start {
             }
         }
         Entity player;
-        if(PickCharacter.heroChoice == 1){
+        if (PickCharacter.heroChoice == 1) {
             player = new Rogue();
-        } else if(PickCharacter.heroChoice == 2){
+        } else if (PickCharacter.heroChoice == 2) {
             player = new Warrior();
-        } else if(PickCharacter.heroChoice == 3){
+        } else if (PickCharacter.heroChoice == 3) {
             player = new Archer();
-        } else if(PickCharacter.heroChoice == 4){
+        } else if (PickCharacter.heroChoice == 4) {
             player = new Mage();
         } else {
             player = new Paladin();
         }
+        
         Game game = new Game(player);
-
-        RandomMonsterMap.setFrame(new RandomMonsterMap(player, Game.spawnRandom(player),textPane, consoleToGUI, frame, game, scrollPane));
+        textPane.setText("");
+        displayIntro(consoleToGUI, scrollPane, textPane);
+        RandomMonsterMap.setFrame(new RandomMonsterMap(player, Game.spawnRandom(player), textPane, consoleToGUI, frame,
+                game, scrollPane));
         RandomMonsterMap.getMapFrame().setVisible(true);
         frame.setVisible(false);
+    }
+
+    public static void displayIntro(ConsoleToGUI consoleToGUI, JScrollPane scrollPane, JTextPane textPane) {
+        System.out.println("\r\n" + //
+                "In the age of Zelario, a realm steeped in magic and mystery, a prophecy unfolds. Legends speak of an ancient artifact, the Heartstone, hidden deep within the Forbidden Forest. This mystical gem is said to hold unimaginable power, capable of shaping the destiny of the entire realm.\r\n"
+                + //
+                "\r\n" + //
+                "You, a humble adventurer seeking fame and fortune, have heard whispers of the Heartstone's existence. Driven by a desire for glory, you embark on a perilous journey to uncover the truth behind the legends. As you delve into the Forbidden Forest, you soon realize that dark forces also seek the Heartstone for nefarious purposes.\r\n"
+                + //
+                "\r\n" + //
+                "Guided by the words of a wise old sage, you must slay foes and gather EXP to be strong enough to reach the Heartstone's secret location.\r\n"
+                + //
+                "\r\n" + //
+                "Your journey is a race against time, as the shadows of an ancient evil gather strength. The fate of Zelario rests on your shoulders. Will you emerge as the hero foretold by prophecy, or will darkness prevail? The adventure awaits, brave soul, as you step into the mystical world of Zelario and embark on a quest that will shape the destiny of an entire realm."); 
+        try {
+            StyledDocument doc = textPane.getStyledDocument();
+            doc.insertString(doc.getLength(), "\nPress Enter to continue.\n", ColorAttributes.DARK_GRAY);
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+        String choice = "";
+        try {
+            choice = consoleToGUI.getNextInput();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        consoleToGUI.scrollToBottom(scrollPane);
     }
 }
