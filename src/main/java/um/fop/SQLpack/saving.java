4a11c6f3e53312e2271b276004cf853bddcbc185
Austin-ty <austin.ty.chen@gmail.com>
2024-01-05 20:56:51 +0800

    这里写上您的提交信息

--- src/main/java/um/fop/SQLpack/saving.java
+++ src/main/java/um/fop/SQLpack/saving.java
@@ -1,70 +1,85 @@
 package SQLpack;
-import java.io.*; // 导入Java I/O库中所有的类，用于文件操作和对象序列化
-import Entitypack.Entity; // 从Entitypack包导入Entity类
 
-// 游戏进度类，包含玩家实体和位置信息
+import java.io.*;
+import Entitypack.Entity;
+
+
 class GameProgress implements Serializable {
-    private Entity playerEntity; // 玩家实体对象
-    private int xPosition; // 玩家的X坐标
-    private int yPosition; // 玩家的Y坐标
+    private Entity playerEntity;
+    private int xPosition;
+    private int yPosition;
 
-    // GameProgress的构造方法
     public GameProgress(Entity playerEntity, int xPosition, int yPosition) {
-        this.playerEntity = playerEntity; // 设置玩家实体
-        this.xPosition = xPosition; // 设置玩家X坐标
-        this.yPosition = yPosition; // 设置玩家Y坐标
+        this.playerEntity = playerEntity;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
     }
 
-    // 将游戏进度信息转换为文本格式的方法
+
     public String toTextFormat() {
-        // 使用字符串拼接生成文本格式的游戏进度信息
         return "Player Entity: " + playerEntity.getName() + "\n" +
                 "HP: " + playerEntity.getHP() + "\n" +
                 "MP: " + playerEntity.getMP() + "\n" +
-                "X Position: " + xPosition + "\n" +
-                "Y Position: " + yPosition;
+                "Level: " + playerEntity.getLevel() + "\n" +
+                "X Position: " + getXPosition() + "\n" +
+                "Y Position: " + getYPosition() + "\n" +
+                // 新增的属性
+                "Name: " + playerEntity.getName() + "\n" +
+                "Max HP: " + playerEntity.getMaxHP() + "\n" +
+                "Max MP: " + playerEntity.getMaxMP() + "\n" +
+                "Physical Attack: " + playerEntity.getPhysicalAttack() + "\n" +
+                "Magical Attack: " + playerEntity.getMagicalAttack() + "\n" +
+                "Physical Defence: " + playerEntity.getPhysicalDefence() + "\n" +
+                "Magical Defence: " + playerEntity.getMagicalDefence();
     }
 
-    // getter和setter方法
-    public Entity getPlayerEntity() { return playerEntity; }
-    public int getXPosition() { return xPosition; }
-    public int getYPosition() { return yPosition; }
+
+
+    public Entity getPlayerEntity() {
+        return playerEntity;
+    }
+
+    // 获取X坐标的方法
+    public int getXPosition() {
+        return xPosition;
+    }
+
+    // 获取Y坐标的方法
+    public int getYPosition() {
+        return yPosition;
+    }
 }
 
 // 用于保存游戏进度的类
 class SaveGame {
-    // 将游戏进度保存为文本格式
-    public void saveGameProgressAsText(GameProgress progress, String filePath) {
-        // 使用try-with-resources语句确保PrintWriter最终会被关闭
-        try (PrintWriter out = new PrintWriter(filePath)) {
-            out.print(progress.toTextFormat()); // 写入游戏进度到文件
-            
-            // 保存Entity的额外属性
-            Entity playerEntity = progress.getPlayerEntity();
-            out.println("Name: " + playerEntity.getName());
-            out.println("Max HP: " + playerEntity.getMaxHP());
-            out.println("Max MP: " + playerEntity.getMaxMP());
-            out.println("Physical Attack: " + playerEntity.getPhysicalAttack());
-            out.println("Magical Attack: " + playerEntity.getMagicalAttack());
-            out.println("Physical Defence: " + playerEntity.getPhysicalDefence());
-            out.println("Magical Defence: " + playerEntity.getMagicalDefence());
+    public void saveGameProgress(String filePath, GameProgress progress) {
+        // 使用ObjectOutputStream序列化GameProgress对象
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(progress);
         } catch (IOException e) {
-            e.printStackTrace(); // 打印异常信息
+            e.printStackTrace();
+        }
+
+        // 创建一个额外的文本文件来保存Entity的属性
+        String textFilePath = filePath + ".txt"; // 文本文件的路径
+        try (PrintWriter out = new PrintWriter(textFilePath)) {
+            // 获取Entity的属性并保存到文本文件
+            out.println(progress.toTextFormat());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
 
 // 用于加载游戏进度的类
 class LoadGame {
-    // 从二进制文件加载游戏进度
     public GameProgress loadGameProgress(String filePath) {
-        // 使用try-with-resources语句确保输入流最终会被关闭
-        try (FileInputStream fileIn = new FileInputStream(filePath);
-             ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
-            return (GameProgress) objectIn.readObject(); // 读取并返回游戏进度对象
+        // 使用ObjectInputStream反序列化GameProgress对象
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
+            return (GameProgress) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace(); // 打印异常信息
-            return null; // 发生异常时返回null
+            e.printStackTrace();
+            return null;
         }
     }
 }
